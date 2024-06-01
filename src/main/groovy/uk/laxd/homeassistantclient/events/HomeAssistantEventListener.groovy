e30df14abcbf54b9e2405a1ac52f83691fc53784
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2024-06-01 09:24:23 +0100

    Replace custom toString implementation with @ToString

--- src/main/groovy/uk/laxd/homeassistantclient/events/HomeAssistantEventListener.groovy
+++ src/main/groovy/uk/laxd/homeassistantclient/events/HomeAssistantEventListener.groovy
@@ -1,17 +1,12 @@
 package uk.laxd.homeassistantclient.events
 
+import groovy.transform.ToString
 import uk.laxd.homeassistantclient.model.json.event.Event
 
+@ToString
 abstract class HomeAssistantEventListener<E extends Event> {
     int subscriptionId
 
     abstract void handleMessage(E event)
 
-
-    @Override
-    String toString() {
-        "HomeAssistantEventListener{" +
-            "subscriptionId=" + subscriptionId +
-            '}'
-    }
 }
