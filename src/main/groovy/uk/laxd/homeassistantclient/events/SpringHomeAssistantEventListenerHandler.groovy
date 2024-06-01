340d9e0a9f46fe87ee172a9fda08ae3505d5ef67
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2024-06-01 09:24:47 +0100

    Replace spring @Component with @Named

--- src/main/groovy/uk/laxd/homeassistantclient/events/SpringHomeAssistantEventListenerHandler.groovy
+++ src/main/groovy/uk/laxd/homeassistantclient/events/SpringHomeAssistantEventListenerHandler.groovy
@@ -1,12 +1,12 @@
 package uk.laxd.homeassistantclient.events
 
 import jakarta.inject.Inject
+import jakarta.inject.Named
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.stereotype.Component
 import uk.laxd.homeassistantclient.model.json.event.Event
 
-@Component
+@Named
 class SpringHomeAssistantEventListenerHandler implements HomeAssistantEventListenerHandler {
     private static final Logger logger = LoggerFactory.getLogger(SpringHomeAssistantEventListenerHandler.class)
 
