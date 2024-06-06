dab3058b004502fa70f899ddc72b6adcf15f813e
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2024-06-06 13:55:01 +0100

    Add github actions integration tests

---
+++ src/integrationTest/groovy/uk/laxd/homeassistantclient/TestSpec.groovy
@@ -0,0 +1,12 @@
+package uk.laxd.homeassistantclient
+
+import spock.lang.Specification
+
+class TestSpec extends Specification {
+
+    def "Integration test"() {
+        expect:
+        true
+    }
+
+}
