554b9026ae1f9f724ad3e1c2179c446e9d050491
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2024-06-11 20:32:07 +0100

    Added first proper integration test

--- src/integrationTest/groovy/uk/laxd/homeassistantclient/EntityIntegrationTest.groovy
+++ src/integrationTest/groovy/uk/laxd/homeassistantclient/EntityIntegrationTest.groovy
@@ -3,8 +3,9 @@ package uk.laxd.homeassistantclient
 import spock.lang.Specification
 import uk.laxd.homeassistantclient.client.HomeAssistant
 import uk.laxd.homeassistantclient.client.HomeAssistantClient
+import uk.laxd.homeassistantclient.model.domain.entity.light.LightEntity
 
-class TestSpec extends Specification {
+class EntityIntegrationTest extends Specification {
 
     String url = "http://localhost:8123"
     String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2YTliNmQwNTcyZjI0ZTZjODlhNmQwYjUxODA3MTU4NiIsImlhdCI6MTY5MjM1Mjk4NSwiZXhwIjoyMDA3NzEyOTg1fQ.sdnC7d7vjiPhWnV2xLQapwc91OuDcTQBDj6DnHapd1Y"
@@ -16,12 +17,21 @@ class TestSpec extends Specification {
             .connect(url, token)
     }
 
-    def "Basic ping test smoke test"() {
+    def "Test entity state returned"() {
         when:
-        def pong = client.ping()
+        def kitchenLight = client.getEntity("light.kitchen", LightEntity)
+        kitchenLight.turnOff()
+        Thread.sleep(10)
 
         then:
-        pong.response == "pong"
+        client.getEntity("light.kitchen", LightEntity).state == 'off'
+
+        when:
+        kitchenLight.turnOn()
+        Thread.sleep(10)
+
+        then:
+        client.getEntity("light.kitchen", LightEntity).state == 'on'
     }
 
 }
