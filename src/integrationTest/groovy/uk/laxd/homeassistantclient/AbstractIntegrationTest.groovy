28fdca8ded3c74623458ad0237f401ab8ef3b0de
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2024-06-13 13:30:05 +0100

    Add proper wait for HA container to come up by listening to logs.

--- src/integrationTest/groovy/uk/laxd/homeassistantclient/AbstractIntegrationTest.groovy
+++ src/integrationTest/groovy/uk/laxd/homeassistantclient/AbstractIntegrationTest.groovy
@@ -1,19 +1,24 @@
 package uk.laxd.homeassistantclient
 
+import groovy.transform.InheritConstructors
 import groovy.util.logging.Slf4j
 import org.testcontainers.containers.BindMode
 import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.wait.strategy.Wait
+import org.testcontainers.containers.wait.strategy.WaitStrategy
+import org.testcontainers.containers.wait.strategy.WaitStrategyTarget
 import org.testcontainers.spock.Testcontainers
 import spock.lang.Specification
 import uk.laxd.homeassistantclient.client.HomeAssistant
 import uk.laxd.homeassistantclient.client.HomeAssistantClient
 
-import java.util.concurrent.TimeoutException
+import java.time.Duration
 
 @Slf4j
 @Testcontainers
 abstract class AbstractIntegrationTest extends Specification {
 
+    String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2YTliNmQwNTcyZjI0ZTZjODlhNmQwYjUxODA3MTU4NiIsImlhdCI6MTY5MjM1Mjk4NSwiZXhwIjoyMDA3NzEyOTg1fQ.sdnC7d7vjiPhWnV2xLQapwc91OuDcTQBDj6DnHapd1Y"
     private HomeAssistantClient client
 
     def version = System.getProperty("HA_VERSION", "latest")
@@ -21,11 +26,11 @@ abstract class AbstractIntegrationTest extends Specification {
     GenericContainer container = new GenericContainer<>("homeassistant/home-assistant:$version")
             .withExposedPorts(8123)
             .withClasspathResourceMapping("test_config", "/config", BindMode.READ_WRITE)
+            .waitingFor(Wait.forLogMessage(".*\\[homeassistant\\.core\\] Starting Home Assistant.*", 1))
 
     HomeAssistantClient getClient() {
         if (!this.client) {
             String url = "http://${container.host}:${container.firstMappedPort}"
-            String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2YTliNmQwNTcyZjI0ZTZjODlhNmQwYjUxODA3MTU4NiIsImlhdCI6MTY5MjM1Mjk4NSwiZXhwIjoyMDA3NzEyOTg1fQ.sdnC7d7vjiPhWnV2xLQapwc91OuDcTQBDj6DnHapd1Y"
 
             log.info("Creating new HA client - $url")
 
@@ -36,14 +41,19 @@ abstract class AbstractIntegrationTest extends Specification {
     }
 
     void waitUntil(Closure condition, retry=5) {
-        retry.times {
+        for (i in 0..retry) {
             if (condition()) {
                 return
             }
             Thread.sleep(10)
         }
 
-        throw new TimeoutException()
+        throw new MaxRetriesExceededException("Retried condition $retry times but was never successful")
     }
+
+}
+
+@InheritConstructors
+class MaxRetriesExceededException extends RuntimeException {
 
 }
