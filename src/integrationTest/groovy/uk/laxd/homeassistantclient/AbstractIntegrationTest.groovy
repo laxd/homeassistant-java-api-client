561cde395c6124fcfc399d56b11a078db6153c8e
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2024-06-13 09:01:36 +0100

    Implement initial testcontainers implementation

--- src/integrationTest/groovy/uk/laxd/homeassistantclient/AbstractIntegrationTest.groovy
+++ src/integrationTest/groovy/uk/laxd/homeassistantclient/AbstractIntegrationTest.groovy
@@ -1,19 +1,33 @@
 package uk.laxd.homeassistantclient
 
+import groovy.util.logging.Slf4j
+import org.testcontainers.containers.BindMode
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.spock.Testcontainers
 import spock.lang.Specification
 import uk.laxd.homeassistantclient.client.HomeAssistant
 import uk.laxd.homeassistantclient.client.HomeAssistantClient
 
 import java.util.concurrent.TimeoutException
 
+@Slf4j
+@Testcontainers
 abstract class AbstractIntegrationTest extends Specification {
 
     private HomeAssistantClient client
 
+    def version = System.getProperty("HA_VERSION", "latest")
+
+    GenericContainer container = new GenericContainer<>("homeassistant/home-assistant:$version")
+            .withExposedPorts(8123)
+            .withClasspathResourceMapping("test_config", "/config", BindMode.READ_WRITE)
+
     HomeAssistantClient getClient() {
         if (!this.client) {
-            String url = "http://localhost:8123"
+            String url = "http://${container.host}:${container.firstMappedPort}"
             String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2YTliNmQwNTcyZjI0ZTZjODlhNmQwYjUxODA3MTU4NiIsImlhdCI6MTY5MjM1Mjk4NSwiZXhwIjoyMDA3NzEyOTg1fQ.sdnC7d7vjiPhWnV2xLQapwc91OuDcTQBDj6DnHapd1Y"
+
+            log.info("Creating new HA client - $url")
 
             this.client = HomeAssistant.createClient().connect(url, token)
         }
