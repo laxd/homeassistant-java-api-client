d3b6e311e7df2a448eb965ef01c85e4ae5d5f1b9
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2024-06-21 14:03:25 +0100

    Added some javadoc to ResponseWebSocketMessage to detail what it does

--- src/main/groovy/uk/laxd/homeassistantclient/model/json/ws/incoming/ResponseWebSocketMessage.groovy
+++ src/main/groovy/uk/laxd/homeassistantclient/model/json/ws/incoming/ResponseWebSocketMessage.groovy
@@ -2,6 +2,10 @@ package uk.laxd.homeassistantclient.model.json.ws.incoming
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
+/**
+ * A message in response to a {@link uk.laxd.homeassistantclient.model.json.ws.outgoing.SubscriptionWebSocketMessage}
+ * that we sent previously.
+ */
 abstract class ResponseWebSocketMessage extends IncomingWebSocketMessage {
 
     @JsonProperty("id")
