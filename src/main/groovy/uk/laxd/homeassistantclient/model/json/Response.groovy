8dc7b1508d859ee338270c28b5617ee8cffbb709
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2024-06-21 14:08:04 +0100

    Create initial Response super class

---
+++ src/main/groovy/uk/laxd/homeassistantclient/model/json/Response.groovy
@@ -0,0 +1,9 @@
+package uk.laxd.homeassistantclient.model.json
+
+/**
+ * Represents a response from the Home Assistant server, either to a listener/trigger that
+ * we set up (Event or Trigger), or to a message that we sent.
+ */
+class Response {
+
+}
--- src/main/groovy/uk/laxd/homeassistantclient/model/json/event/Event.groovy
+++ src/main/groovy/uk/laxd/homeassistantclient/model/json/event/Event.groovy
@@ -3,11 +3,15 @@ package uk.laxd.homeassistantclient.model.json.event
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import uk.laxd.homeassistantclient.model.json.Response
 import uk.laxd.homeassistantclient.model.json.event.jackson.EventDeserialiser
 
+/**
+ * Represents an event originating from the server in response to a trigger, or state change
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonDeserialize(using = EventDeserialiser)
-class Event {
+class Event extends Response {
 
     @JsonProperty("context")
     Map<String, Object> context
