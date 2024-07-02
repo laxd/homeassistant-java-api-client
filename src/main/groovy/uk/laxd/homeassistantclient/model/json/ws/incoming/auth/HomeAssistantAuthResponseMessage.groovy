06b4606970bc15eb89a3c191f7ce12a76775496e
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2024-07-02 23:22:15 +0100

    Add some basic javadocs to HomeAssistantAuthResponseMessage

--- src/main/groovy/uk/laxd/homeassistantclient/model/json/ws/incoming/auth/HomeAssistantAuthResponseMessage.groovy
+++ src/main/groovy/uk/laxd/homeassistantclient/model/json/ws/incoming/auth/HomeAssistantAuthResponseMessage.groovy
@@ -1,7 +1,13 @@
 package uk.laxd.homeassistantclient.model.json.ws.incoming.auth
 
+/**
+ * A response to an authentication attempt, e.g. a {@code AuthenticationWebSocketMessage}
+ */
 abstract class HomeAssistantAuthResponseMessage extends HomeAssistantAuthMessage {
 
+    /**
+     * A potential message for e.g. authentication failures.
+     */
     String message
 
     abstract boolean isSuccessful()
