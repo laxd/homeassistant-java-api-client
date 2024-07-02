22681dc7a1a794794b446d2ea911f073023d4018
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2024-07-03 00:09:32 +0100

    Updated all LightEntity methods to wait for future before returning

--- src/main/groovy/uk/laxd/homeassistantclient/model/domain/entity/light/LightEntity.groovy
+++ src/main/groovy/uk/laxd/homeassistantclient/model/domain/entity/light/LightEntity.groovy
@@ -3,11 +3,13 @@ package uk.laxd.homeassistantclient.model.domain.entity.light
 import groovy.transform.InheritConstructors
 import uk.laxd.homeassistantclient.model.domain.entity.GenericEntity
 
+import java.util.concurrent.TimeUnit
+
 @InheritConstructors
 class LightEntity extends GenericEntity {
 
     void setBrightness(short brightness) {
-        this.wsClient.turnOn(this.entityId, ["brightness": brightness])
+        this.wsClient.turnOn(this.entityId, ["brightness": brightness]).get(10, TimeUnit.SECONDS)
     }
 
     short getBrightness() {
@@ -27,7 +29,7 @@ class LightEntity extends GenericEntity {
      * @param colourTemperature
      */
     void setColourTemperature(short colourTemperature) {
-        this.wsClient.turnOn(this.entityId, ["color_temp": colourTemperature])
+        this.wsClient.turnOn(this.entityId, ["color_temp": colourTemperature]).get(10, TimeUnit.SECONDS)
     }
 
     Colour getColour() {
@@ -37,7 +39,7 @@ class LightEntity extends GenericEntity {
     }
 
     void setColour(Colour colour) {
-        this.wsClient.turnOn(this.entityId, ["rgb_color": [colour.red, colour.green, colour.blue]])
+        this.wsClient.turnOn(this.entityId, ["rgb_color": [colour.red, colour.green, colour.blue]]).get(10, TimeUnit.SECONDS)
     }
 
 }
